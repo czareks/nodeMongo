7d6b9d2df3d563b5aa0dc634efe8f683882d31e9
czareks <czarek1gplay@gmail.com>
2022-11-16 19:38:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,49 +11,6 @@ app.get("/", (req, res) => {
   res.send("Serwer działa!");
 });
 
-app.post("/todos", (req, res) => {
-  todoList.push(req.body);
-  res.status(200).end();
-});
-
-app.get("/todos", (req, res) => {
-  res.json({ todoList });
-});
-
-app.delete("/todos/:todoId", (req, res) => {
-  const todoId = parseInt(req.params.todoId, 10);
-
-  const todoItemIndex = todoList.findIndex((e) => e.id === todoId);
-
-  if (todoItemIndex !== undefined) {
-    todoList.splice(todoItemIndex, 1);
-    res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.patch("/todos/:todoId", (req, res) => {
-  const todoId = parseInt(req.params.todoId, 10);
-  const todoItem = todoList.find((e) => e.id === todoId);
-
-  if (todoItem) {
-    const update = req.body;
-
-    if (update.completed !== undefined) {
-      todoItem.completed = update.completed;
-    }
-
-    if (update.title !== undefined) {
-      todoItem.title = update.title;
-    }
-
-    res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
-});
-
 const port = process.env.PORT || 8888;
 
 app.listen(port, () => {
